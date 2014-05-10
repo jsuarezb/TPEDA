102486e11d2c5ba297eaa4512f4050bfc6254782
tomatereloco <tomascllc@hotmail.com>
2014-05-09 23:29:37 -0300

    Board

--- src/model/board/Board.java
+++ src/model/board/Board.java
@@ -4,53 +4,58 @@ import java.awt.Point;
 public class Board {
 	private Tile[][] board;
 	private int lastCol;
-	
+
 	public Board( Tile[][] board ) {
 		this.board = board;
 	}
-	
+
 	public boolean isOver() {
 		for( int y = board.length - 1; y >= 0; y-- )
 			for( int x = 0; x <= lastCol; x++ )
 				if( hasAdyacents(new Point(x,y)) )
 					return false;
+
 		return true;
 	}
-	
+
 	private boolean hasAdyacents(Point pos) {
 		Tile tile = getTile(pos);
-		
+
 		if( tile.equals(getRight(pos)) )
 			return true;
 		if( tile.equals(getTop(pos)) )
 			return true;
 		return false;
 	}
-	
-	private Tile getTile(Point pos) {
+
+	public Tile getTile(Point pos) {
+		if (pos.y < 0 || pos.y >= board.length
+				|| pos.x < 0 || pos.x >= board[0].length)
+			return null;
+
 		return board[pos.y][pos.x];
 	}
-	
+
 	private Tile getRight(Point pos) {
 		if( pos.x + 1 >= board[0].length )
 			return null;
 		return board[pos.y][pos.x + 1];
 	}
-	
+
 	private Tile getTop(Point pos) {
 		if( pos.y - 1 < 0 )
 			return null;
 		return board[pos.y - 1][pos.x];
 	}
-	
+
 	private void gravity() {
 		for( int x = 0; x <= lastCol; x++ ){
 			int spaces = 0;
-			
+
 			for( int y = board.length - 1; y >= 0; y-- ){
 				Point pos = new Point(x,y);
 				Tile tile = getTile(pos);
-				
+
 				if( tile.isEmpty() )
 					spaces++;
 				else
@@ -58,14 +63,67 @@ public class Board {
 			}
 		}
 	}
-	
+
 	private void drop(Point pos, int spaces) {
-		board[pos.y][pos.x + spaces] = getTile(pos);
+		board[pos.y + spaces][pos.x] = getTile(pos);
 		board[pos.y][pos.x] = new Tile(' ');
 	}
-	
+
 	private void alignLeft() {
-		for( int x = 0; x <= lastCol; x++ )
-			
+		int lastCol = -1;
+		for( int x = 0; x <= lastCol; x++ ) {
+			Tile bottomTile = board[board.length - 1][x];
+			if (!bottomTile.isEmpty()){				
+				if (x - 1 >= 0 && board[board.length - 1][x - 1].isEmpty()) {
+					for (int y = board.length - 1; y >= 0; y--) {
+						board[y][lastCol + 1] = board[y][x];
+						board[y][x] = new Tile(' ');
+					}
+				}
+
+				lastCol++;
+			}
+		}
+	}
+
+	public int delete(Point pos) {
+		boolean hasAdj = false;
+		int[][] moves = new int[][]{ {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+		Point point;
+		Tile tile = getTile(pos);
+
+		if (tile == null || tile.isEmpty())
+			throw new IllegalArgumentException("Invalid tile");
+
+		for (int i = 0; i < moves.length && !hasAdj; i++) {
+			point = new Point(pos.x + moves[i][0], pos.y + moves[i][1]);
+			if (tile.equals(getTile(point)))
+				hasAdj = true;
+
+		}
+
+		if (!hasAdj)
+			return 0;
+
+		return deleteTile(pos);
+	}
+
+	private int deleteTile(Point pos) {
+		int tilesModified = 0;
+		int[][] moves = new int[][]{ {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+		Point point;
+		Tile tile = getTile(pos), adjTile;
+
+		for (int i = 0; i < moves.length; i++) {
+			point = new Point(pos.x + moves[i][0], pos.y + moves[i][1]);
+			adjTile = getTile(point);
+			if (adjTile != null) {
+				if (tile.equals(adjTile)) {
+					tilesModified += delete(point);
+				}
+			}
+		}
+
+		return tilesModified + 1;
 	}
 }
